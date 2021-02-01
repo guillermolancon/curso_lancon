8c4dd5ec066e5f1748b733c60ca3cb88ba053c81
Guillermo <guillelanas@gmail.com>
2021-02-01 17:04:05 -0300

    'tp2'

---
+++ src/Controller/Tp2Controller.php
@@ -0,0 +1,22 @@
+<?php
+// src/Controller/UserController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Tp2Controller extends AbstractController
+{
+     /**
+      * @Route("/tp2")
+      */
+
+    public function notifications(): Response
+    {   $userFirstName = TRUE;
+        return $this->render('tp2.html.twig',[
+            'salio' => $userFirstName,
+            
+        ]);
+    }
+}
